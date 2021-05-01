8b0aa3d06fe32f294902a7ee4bb9361c531b7141
Sam Revanoor <sam@trint.com>
2021-05-01 16:26:28 -0400

    completes chuck norris lab, hardcoded categories so working on rendering dynamically

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const request = require('request');
+const token = process.env.GITHUB_TOKEN;
+const rootURL = 'https://api.chucknorris.io/jokes/';
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  const category = req.query.category;
+  const options = {
+    url: `${rootURL}random?category=${category}`,
+    headers: {
+      'User-Agent': 'samrevanoor',
+      Authorization: `token ${token}`
+    }
+  };
+  request(options, function(err, response,body){
+    console.log(`category: ${category}`);
+    const jokeData = JSON.parse(body);
+    res.render('index', { categories:["dev", "music"], jokeData });
+  })
 });
 
 module.exports = router;
